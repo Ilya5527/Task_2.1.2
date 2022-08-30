91251cd45af350601eb9ab394c1bf8bb3e0dc6b0
Ilya5527 <kyzmin.ilya2014@ynadex.ru>
2022-08-30 08:31:39 +0300

    first commit

---
+++ src/main/java/App.java
@@ -0,0 +1,27 @@
+import model.Cat;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+//        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Сравниванием бины HelloWorld, у которых скоуп Синглтон");
+        System.out.println(bean == bean1);
+
+        System.out.println("Сравниваем бины Cat, у которых скоп Прототайп");
+        System.out.println(catBean1 == catBean2);
+
+
+
+    }
+}
---
+++ src/main/java/model/Cat.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Cat {
+
+    private String name;
+    private byte age;
+
+    public Cat() {
+    }
+
+    public Cat(String name, byte age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
